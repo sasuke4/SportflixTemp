c57e0dc6d1e73d77f0f8fb3163411a206b46b86e
Joshua Torres <jtorres@lnphi.com>
2017-02-13 20:30:40 -0600

    Empecé a conectar la aplicacion con redux

--- app/main.js
+++ app/main.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Landing from './components/Landing/Landing';
 import { render } from 'react-dom';
+import { Provider } from 'redux';
+import store from './store';
 import './../sass/landing.scss';
 import './../sass/modal.scss';
 const location = window.location.origin;
 
 render(
-  <Landing api='http://localhost:8000' location={ location } />,
+  <Provider>
+    <Landing api='http://localhost:8000' location={ location } />
+  </Provider>,
   document.querySelector('.app')
 );
