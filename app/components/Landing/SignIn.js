bfc066b0b75d7b9a2b259681eae1bf36cd082c86
Joshua Torres <jtorres@lnphi.com>
2017-02-13 19:46:13 -0600

    Agregué el servicio del inicio de sesión

--- app/components/Landing/SignIn.js
+++ app/components/Landing/SignIn.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { request } from '../../helpers/fetch-server.js';
 
 export default React.createClass({
   displayName: 'SignIn',
   mixins: [ PureRenderMixin ],
+  propTypes: {
+    api: PropTypes.string.isRequired,
+  },
+  onclick() {
+    const { api, closeModal } = this.props;
+    request({
+      url: `${ api }/api/users/signin/`,
+      method: 'post',
+      body: new FormData(this.refs.form),
+    }).then(response => {
+      console.log(response.payload);
+      closeModal();
+    }).catch(error =>
+      console.log(error)
+    );
+  },
   render() {
     return (
-      <div className='modal-input'>
-        <input placeholder='Correo Electrónico' className='modal-input__input'/>
-        <input type='password' placeholder='Contraseña' className='modal-input__input'/>
-        <button className='button button--gray' type="button" >Iniciar Sesión</button>
-      </div>
+      <form className='modal-input' ref='form'>
+        <input name='email' placeholder='Correo Electrónico' className='modal-input__input'/>
+        <input name='password' type='password' placeholder='Contraseña' className='modal-input__input'/>
+        <button className='button button--gray' type="button" onClick={ this.onclick } >Iniciar Sesión</button>
+      </form>
     );
   },
 });
