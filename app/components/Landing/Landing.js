0965085bc6f12b73ce8969b380a12e9677b8a848
Joshua Torres <jtorres@lnphi.com>
2017-02-15 16:35:21 -0600

    Agregué el filtro blur

--- app/components/Landing/Landing.js
+++ app/components/Landing/Landing.js
@@ -45,36 +45,39 @@ export default React.createClass({
     const { header, headline_1, headline_2, headline_3, image1, image2, image3, subscription_plans = [] } = data;
     const { api, location } = this.props;
     const actualModal = modalType({ api, closeModal: this.closeModal, switchModal: this.switchModal, currentModal });
+    const classNameLanding = showModal ? 'landing landing--blur' : 'landing';
     return (
-      <div className='landing'>
+      <div className='landing-container'>
         <Modal closeModal={ this.closeModal } show={ showModal } location={ location } >
           { actualModal }
         </Modal>
-        <div className='landing-top'>
-          <div className='landing-top__logo'>SPORTFLIX</div>
-          <button name='signin' className='landing-top__sign-in' onClick={ this.openModal }>INICIAR SESIÓN</button>
-        </div>
-        <div className='landing-header'>
-          <img className='landing-header__img' src={ `${ api }${ image1 }` } alt="header-img" />
-          <div className='landing-header__header'>{ header }</div>
-          <div className='landing-header-left'>
-            <div className='landing-header-left__headline-1'>{ headline_1 }</div>
+        <div className={ classNameLanding }>
+          <div className='landing-top'>
+            <div className='landing-top__logo'>SPORTFLIX</div>
+            <button name='signin' className='landing-top__sign-in' onClick={ this.openModal }>INICIAR SESIÓN</button>
+          </div>
+          <div className='landing-header'>
+            <img className='landing-header__img' src={ `${ api }${ image1 }` } alt="header-img" />
+            <div className='landing-header__header'>{ header }</div>
+            <div className='landing-header-left'>
+              <div className='landing-header-left__headline-1'>{ headline_1 }</div>
+              <button name='signup' className='button' type="button" onClick={ this.openModal }>REGÍSTRATE</button>
+            </div>
+          </div>
+          <div className='plans-container'>
+            <div className='plans-container__headline-2'>{ headline_2 }</div>
+            <div className='plans-container__headline-3'>{ headline_3 }</div>
+            <Plans data={ subscription_plans } />
             <button name='signup' className='button' type="button" onClick={ this.openModal }>REGÍSTRATE</button>
           </div>
-        </div>
-        <div className='plans-container'>
-          <div className='plans-container__headline-2'>{ headline_2 }</div>
-          <div className='plans-container__headline-3'>{ headline_3 }</div>
-          <Plans data={ subscription_plans } />
-          <button name='signup' className='button' type="button" onClick={ this.openModal }>REGÍSTRATE</button>
-        </div>
-        <div className='landing-banner'>
-          <img className='landing-banner__img' src={ `${ api }${ image2 }` } alt="header-img" />
-          <div className='landing-banner__text'>DONDE QUIERAS SIN ANUNCIOS EN LA MAS ALTA CALIDAD</div>
-        </div>
-        <div className='landing-banner'>
-          <img className='landing-banner__img landing-banner__img--big' src={ `${ api }${ image3 }` } alt="header-img" />
-          <div className='landing-banner__text landing-banner__text--big'>DONDE QUIERAS SIN ANUNCIOS EN LA MAS ALTA CALIDAD</div>
+          <div className='landing-banner'>
+            <img className='landing-banner__img' src={ `${ api }${ image2 }` } alt="header-img" />
+            <div className='landing-banner__text'>DONDE QUIERAS SIN ANUNCIOS EN LA MAS ALTA CALIDAD</div>
+          </div>
+          <div className='landing-banner'>
+            <img className='landing-banner__img landing-banner__img--big' src={ `${ api }${ image3 }` } alt="header-img" />
+            <div className='landing-banner__text landing-banner__text--big'>DONDE QUIERAS SIN ANUNCIOS EN LA MAS ALTA CALIDAD</div>
+          </div>
         </div>
       </div>);
   },
