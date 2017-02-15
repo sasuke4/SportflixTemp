dea7f638aaf8d543b88bb207326392db47ed4101
Joshua Torres <jtorres@lnphi.com>
2017-02-14 18:27:14 -0600

    Eliminé metodos innecesarios

--- app/components/Landing/SignIn.js
+++ app/components/Landing/SignIn.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
-import { request } from 'helpers/fetch-server.js';
+import { request } from 'helpers/fetch-server';
 import { setSesion } from 'state/actions';
 
 export default React.createClass({
@@ -23,19 +23,12 @@ export default React.createClass({
       console.log(error)
     );
   },
-  onclic() {
-    console.log(this.props.token);
-  },
-  componentWillMount() {
-    console.log(this.props.token);
-  },
   render() {
     return (
       <form className='modal-input' ref='form'>
         <input name='email' placeholder='Correo Electrónico' className='modal-input__input'/>
         <input name='password' type='password' placeholder='Contraseña' className='modal-input__input'/>
         <button className='button button--gray' type="button" onClick={ this.onclick } >Iniciar Sesión</button>
-        <button className='button button--gray' type="button" onClick={ this.onclic } >Props</button>
       </form>
     );
   },
